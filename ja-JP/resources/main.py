c456bb3e033a4dbda0219c31814488a77ec15880
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-03-07 10:49:12 +0000

    New translations main.py (Japanese)

--- ja-JP/resources/main.py
+++ ja-JP/resources/main.py
@@ -4,43 +4,43 @@ from emoji import *
 from datetime import *
 from random import randint
 
-# Put function definitions under here
+＃関数の定義をこの下に書く
 
 def roll_dice():
-  print(python, 'can make a', dice)
-  max = input('How many sides?') # get input from the user
-  print('That\'s a D', max) # use the number the user entered
-  roll = randint(1, int(max)) # generate a random number 
-  print('You rolled a', roll) # print the value of the roll variable
-  print(fire * roll) # repeat the fire text roll times
+  print(python, 'で', dice, 'を作ることができます')
+  max = input('何面にしますか?') # ユーザーに入力してもらう
+  print(max, '面ですね') # ユーザーが入力した数値を使う
+  roll = randint(1, int(max)) # ランダムな数値を生成する 
+  print('サイコロの目はこれでした', roll) # 変数rollの値を出力する
+  print(fire * roll) # サイコロの目の回数だけ炎の文字を繰り返し出力する
 
 def hobbies():
-  hobby = input('What do you like?')
-  print('That sounds', fun)
-  print('You could make a', python, 'project about', hobby)
+  hobby = input('あなたは何に興味がありますか?')
+  print('それはとても', fun, 'そうですね')
+  print('あなたは', hobby, 'に関する', python, 'プロジェクトを作ることができます')
 
-# Useful characters :',()*_/.#
+# 使える文字 :',()*_/.#
 
-# Put code to run under here
-print('Hello', world)
-print('Welcome to', python)
+# 動かしたいコードをこの下に書く
+print('こんにちは', world)
+print(python, 'へ ようこそ')
 
-input() # wait for the user to tap Enter
+input() # ユーザーがEnterを たたくのを待つ
 
-print(python, 'is very good at', sums)
+print(python, 'は', sums, 'が得意です')
 print(230 * 5782 ** 2 / 23781)
 
 input()
 
-print('The', calendar, clock, 'is', datetime.now()) # print current date and time with emoji 
+print(calendar, clock, 'は', datetime.now()) # 現在の日付と時刻を絵文字と一緒に出力 
 
 input()
 
-roll_dice() # Call the roll_dice function
+roll_dice() # roll_dice関数を呼び出す
 
 input()
 
-hobbies() # Call the hobbies function
+hobbies() # hobbies関数を呼び出す
 
 
 
