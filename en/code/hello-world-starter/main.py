821ffc94b2bad858de258eecd21f21dc96564f86
tessyraspberrypi <136448689+tessyraspberrypi@users.noreply.github.com>
2023-10-18 14:30:54 +0100

    update yml

---
+++ en/code/hello-world-starter/main.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from random import randint
+
+# Emoji variables to use in your project
+world = '🌍🌎🌏'
+python = 'Python 🐍'
+fire = '🔥'
+
+# Emojis to copy and paste into your code 
+# 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒🎨🎮🔬🎉🕶️🎲 😊
+# 👩‍🦽👩🏼‍🦽👩🏽‍🦽👩🏾‍🦽👩🏿‍🦽🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋🙋🏼🙋🏽🙋🏾🙋🏿
+# 🦄🚀💯⭐💛❤️📚⚽🏏🏀🥋🏆✨🥺🌈🔥♻️🌳
+
+# Useful characters :',()*_/.#
+
+# Function definitions        
+  
+# Put code to run under here
