4b921c750256706dceb18c4978a5180d40551b68
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-05 14:31:57 +0000

    New translations main.py (Italian)

--- it-IT/code/hello-world-example/main.py
+++ it-IT/code/hello-world-example/main.py
@@ -1,31 +1,33 @@
 
 
 
+# Variabili emoji da utilizzare nel tuo progetto
+mondo = '🌍🌎🌏'
+python = 'Python 🐍'
+fuoco = '🔥'
+
+# Emoji da copiare e incollare nel tuo codice:
 
 
+# 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
+# Caratteri utili :',()*_/.#
 
+# Definizione delle funzioni
+def tira_dado():
+    massimo = input('Quanti lati?:) # Aspetta l'input dell'utente
+    print('Questo è un D', massimo) # Usa il numero che l'utente ha inserito
+    risultato = randint(1, int(massimo)) # Utilizza il massimo per determinare il numero di lati del dado
+    print('Hai tirato un', risultato, fuoco * risultato) # Ripetil l'emoji fuoco tante volte quante il risultato
 
+# Metti qui sotto il codice che verrà eseguito
+print('Ciao', mondo)
+print('Benvenuti in ', python)
+print(python, 'è molto bravo in matematica!')
+print(230 * 5782 ** 2 / 23781)  # Stampa il risultato della somma
+print('La data e l'ora sono', datetime.now()) # Stampa la data e l'ora corrente
 
-
-
-
-
-
-
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
+tira_dado() # Chiama la funzione per tirare il dado
+print('Io ❤️ gli arcobaleni 🌈')
+print('Gli unicorni 🦄 mi rendono 😃')
+print('Mi piacerebbe creare una storia 📖 con', python)
