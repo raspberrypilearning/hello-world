189ed3693c796f267afa25433f110b29d270059c
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-05 14:32:19 +0000

    New translations main.py (Italian)

--- it-IT/code/hello-world-starter/main.py
+++ it-IT/code/hello-world-starter/main.py
@@ -1,16 +1,18 @@
 
 
 
+# Variabili emoji da utilizzare nel tuo progetto
+mondo = '🌍🌎🌏'
+python = 'Python 🐍'
+fuoco = '🔥'
+
+# Emoji da copiare e incollare nel tuo codice:
 
 
+# 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
+# Caratteri utili :',()*_/.#
 
+# Definizione delle funzioni
 
-
-
-
-
-
-
-
-
+# Metti qui sotto il codice che verrà eseguito
