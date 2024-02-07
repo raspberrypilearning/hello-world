b796019a0bb8c395d5ba8a95ce0533a683a52c41
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-07 12:30:00 +0000

    New translations main.py (Italian)

--- it-IT/code/hello-world-example/main.py
+++ it-IT/code/hello-world-example/main.py
@@ -15,17 +15,17 @@ fuoco = '🔥'
 
 # Definizione delle funzioni
 def tira_dado():
-    massimo = input('Quanti lati?:) # Aspetta l'input dell'utente
+    massimo = input('Quanti lati?:') # Aspetta l'input dell'utente
     print('Questo è un D', massimo) # Usa il numero che l'utente ha inserito
     risultato = randint(1, int(massimo)) # Utilizza il massimo per determinare il numero di lati del dado
-    print('Hai tirato un', risultato, fuoco * risultato) # Ripetil l'emoji fuoco tante volte quante il risultato
+    print('Hai tirato un', risultato, fuoco * risultato) # Ripeti l'emoji fuoco tante volte quante il risultato
 
 # Metti qui sotto il codice che verrà eseguito
 print('Ciao', mondo)
 print('Benvenuti in ', python)
 print(python, 'è molto bravo in matematica!')
 print(230 * 5782 ** 2 / 23781)  # Stampa il risultato della somma
-print('La data e l'ora sono', datetime.now()) # Stampa la data e l'ora corrente
+print('La data e l\'ora sono', datetime.now()) # Stampa la data e l'ora corrente
 
 tira_dado() # Chiama la funzione per tirare il dado
 print('Io ❤️ gli arcobaleni 🌈')
