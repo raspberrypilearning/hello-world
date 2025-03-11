f8974c8d8b5ebe0acec14bd4894082ebdfc74331
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-03-11 09:36:29 +0000

    New translations main.py (Ukrainian)

--- uk-UA/code/hello-world-example/main.py
+++ uk-UA/code/hello-world-example/main.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from random import randint
 
 # Змінні з емоджі для твого проєкту
-world = '🌍🌎🌏'
-python = 'Python 🐍'
-fire = '🔥'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Емоджі, які ти можеш скопіювати у свій код:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
@@ -13,21 +13,25 @@ fire = '🔥'
 
 # Корисні символи :',()*_/.#
 
+
 # Визначення функцій
 def roll_dice():
-    max = input('Кількість сторін кубика:')  # Чекай, доки користувач введе дані
-    print('Цей кубик називається D', max)  # Використай число, введене користувачем
+    max = input("How many sides?:")  # Wait for input from the user
+    print("That's a D", max)  # Use the number the user entered
     roll = randint(1, int(max))  # Використай змінну max, щоб вказати кількість сторін кубика
-    print('Тобі випало число', roll, fire * roll)  # Повтори емоджі вогника стільки разів, скільки тобі випало на кубику
+    print(
+        "You rolled a", roll, fire * roll
+    )  # Repeat the fire emoji to match the dice roll
+
 
 # Нижче розмісти код, який потрібно виконати
-print('Привіт,', world)
-print('Ласкаво просимо до', python)
-print(python, 'знається на математиці!')
-print(230 * 5782 ** 2 / 23781)  # Виведи результат розрахунків
-print('Дата й час —', datetime.now())  # Виведи поточну дату й час
+print("Hello", world)
+print("Welcome to", python)
+print(python, "is very good at maths!")
+print(230 * 5782**2 / 23781)  # Print the result of the sum
+print("The date and time is", datetime.now())  # Print the current date and time
 
 roll_dice()  # Виклич функцію, яка кидає кубик
-print('Я ❤️ веселку 🌈')
-print('Єдинороги 🦄 викликають у мене 😃')
-print('Я хочу написати оповідання 📖 за допомогою', python)
+print("I ❤️ rainbows 🌈")
+print("Unicorns 🦄 make me 😃")
+print("I'd like to make a story 📖 with", python)
