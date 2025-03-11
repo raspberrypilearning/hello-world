371e84a3212ab39b34c9635c1fca29077ff180aa
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-03-11 09:36:20 +0000

    New translations main.py (German)

--- de-DE/code/hello-world-example/main.py
+++ de-DE/code/hello-world-example/main.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from random import randint
 
 # Emoji-Variablen zur Verwendung in Deinem Projekt
-welt = '🌍🌎🌏'
-python = 'Python 🐍'
-feuer = '🔥'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Emojis zum Kopieren und Einfügen in den Code:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
@@ -13,21 +13,25 @@ feuer = '🔥'
 
 # Hilfreiche Zeichen:',()*_/.#
 
+
 # Funktionsdefinitionen
 def wuerfel_werfen():
-    max = input('Wie viele Seiten?:')  # Warte auf Eingabe vom Benutzer
-    print('Das ist ein W', max)  # Verwende die vom Benutzer eingegebene Zahl
+    max = input("How many sides?:")  # Wait for input from the user
+    print("That's a D", max)  # Use the number the user entered
     wurf = randint(1, int(max))  # Verwende max, um die Anzahl der Seiten des Würfels zu bestimmen
-    print('Du hast gewürfelt:', wurf, feuer * wurf) # Wiederhole das Feuer-Emoji so oft wie der Würfelwurf anzeigt
+    print(
+        "You rolled a", roll, fire * roll
+    )  # Repeat the fire emoji to match the dice roll
+
 
 # Füge hier den Code ein, der ausgeführt werden soll
-print('Hallo', welt)
-print('Willkommen bei', python)
-print(python, 'ist sehr gut in Mathe!')
-print(230 * 5782 ** 2 / 23781) # Gib das Ergebnis der Rechnung aus
-print('Datum und Uhrzeit sind', datetime.now())  # Gib aktuelles Datum und Uhrzeit aus
+print("Hello", world)
+print("Welcome to", python)
+print(python, "is very good at maths!")
+print(230 * 5782**2 / 23781)  # Print the result of the sum
+print("The date and time is", datetime.now())  # Print the current date and time
 
 wuerfel_werfen() # Rufe die Funktion zum Würfel werfen auf
-print('Ich ❤️ Regenbögen 🌈')
-print('Einhörner 🦄 machen mich 😃')
-print('Ich möchte eine Geschichte erfinden 📖 mit', Python)
+print("I ❤️ rainbows 🌈")
+print("Unicorns 🦄 make me 😃")
+print("I'd like to make a story 📖 with", python)
