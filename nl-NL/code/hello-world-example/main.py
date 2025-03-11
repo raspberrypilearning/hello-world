da4c4b7ec04a1bbe10af68059c41e43d3237a48d
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-03-11 09:36:16 +0000

    New translations main.py (Dutch)

--- nl-NL/code/hello-world-example/main.py
+++ nl-NL/code/hello-world-example/main.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from random import randint
 
 # Emoji-variabelen die je in je project kunt gebruiken
-wereld = '🌍🌎🌏'
-python = 'Python 🐍'
-vuur = '🔥'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Emoji's om te kopiëren en in je code te plakken:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
@@ -13,21 +13,25 @@ vuur = '🔥'
 
 # Nuttige tekens :',()*_/.#
 
+
 # Functiedefinities
 def gooi_dobbelsteen():
-    max = input('Hoeveel zijden?:') # Wacht op invoer van de gebruiker
-    print('Dat is een D', max) # Gebruik het getal dat de gebruiker heeft ingevoerd
+    max = input("How many sides?:")  # Wait for input from the user
+    print("That's a D", max)  # Use the number the user entered
     worp = randint(1, int(max)) # Gebruik max om het aantal zijden van de dobbelsteen te bepalen
-    print('Je hebt een ', worp, vuur * worp, 'gegooid') # Herhaal de vuur-emoji zodat deze overeenkomt met de dobbelsteenworp
+    print(
+        "You rolled a", roll, fire * roll
+    )  # Repeat the fire emoji to match the dice roll
+
 
 # Zet de code om uit te voeren hieronder
-print('Hallo', wereld)
-print('Welkom bij', python)
-print(python, 'is erg goed in wiskunde!')
-print(230 * 5782 ** 2 / 23781) # Laat het resultaat van de som zien
-print('De datum en tijd is', datetime.now()) # Laat de huidige datum en tijd zien
+print("Hello", world)
+print("Welcome to", python)
+print(python, "is very good at maths!")
+print(230 * 5782**2 / 23781)  # Print the result of the sum
+print("The date and time is", datetime.now())  # Print the current date and time
 
 gooi_dobbelsteen() # Roep de dobbelsteen functie aan
-print('Ik ❤️ regenbogen 🌈')
-print('Eenhoorns 🦄 maken mij 😃')
-print('Ik wil graag een verhaal 📖 maken met', python)
+print("I ❤️ rainbows 🌈")
+print("Unicorns 🦄 make me 😃")
+print("I'd like to make a story 📖 with", python)
