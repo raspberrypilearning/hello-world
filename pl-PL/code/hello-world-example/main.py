44b0f90d73a479dcc4ebdc2c1ff6695c86c044a6
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-04-03 11:39:17 +0100

    New translations main.py (Polish)

--- pl-PL/code/hello-world-example/main.py
+++ pl-PL/code/hello-world-example/main.py
@@ -1,37 +1,37 @@
 from datetime import datetime
 from random import randint
 
-# Emoji variables to use in your project
+# Zmienne emoji do wykorzystania w Twoim projekcie
 world = "🌍🌎🌏"
 python = "Python 🐍"
 fire = "🔥"
 
-# Emojis to copy and paste into your code:
+# Emoji do skopiowania i wklejenia do kodu:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
 # 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽 🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
 # 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
-# Useful characters :',()*_/.#
+# Przydatne znaki :',()*_/.#
 
 
-# Function definitions
+# Definicje funkcji
 def roll_dice():
     max = input("How many sides?:")  # Wait for input from the user
     print("That's a D", max)  # Use the number the user entered
-    roll = randint(1, int(max))  # Use max to determine the number of sides the dice has
+    Rzuć = randint(1, int(max)) # Użyj max, aby określić liczbę boków kości
     print(
         "You rolled a", roll, fire * roll
     )  # Repeat the fire emoji to match the dice roll
 
 
-# Put code to run under here
+# Wstaw kod do uruchomienia tutaj
 print("Hello", world)
 print("Welcome to", python)
 print(python, "is very good at maths!")
 print(230 * 5782**2 / 23781)  # Print the result of the sum
 print("The date and time is", datetime.now())  # Print the current date and time
 
-roll_dice()  # Call the roll dice function
+Roll_dice() # Wywoływanie funkcji kości rzutu
 print("I ❤️ rainbows 🌈")
 print("Unicorns 🦄 make me 😃")
 print("I'd like to make a story 📖 with", python)
