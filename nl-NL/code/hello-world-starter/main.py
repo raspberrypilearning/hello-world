112a97ea8306ca7c898bf6aee6e51433b29778e3
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-10-24 13:44:30 +0100

    New translations main.py (Dutch)

--- nl-NL/code/hello-world-starter/main.py
+++ nl-NL/code/hello-world-starter/main.py
@@ -2,17 +2,19 @@ from datetime import datetime
 from random import randint
 
 # Emoji-variabelen die je in je project kunt gebruiken
-wereld = '🌍🌎🌏'
-python = 'Python 🐍'
-vuur = '🔥'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Emoji's om te kopiëren en in je code te plakken:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
-# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
+# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽 🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
 # 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
 # Nuttige tekens :',()*_/.#
 
+
 # Functiedefinities
+
 
 # Zet de code om uit te voeren hieronder
