66642acebf07ed4c0fefc9ac410585be990fedee
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-10-24 13:44:37 +0100

    New translations main.py (Polish)

--- pl-PL/code/hello-world-starter/main.py
+++ pl-PL/code/hello-world-starter/main.py
@@ -2,17 +2,19 @@ from datetime import datetime
 from random import randint
 
 # Zmienne emoji do wykorzystania w Twoim projekcie
-świat = '???'
-python = 'Python ?'
-ogień = '?'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Emoji do skopiowania i wklejenia do kodu:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
-# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
+# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽 🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
 # 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
 # Przydatne znaki :',()*_/.#
 
+
 # Definicje funkcji
+
 
 # Wstaw kod do uruchomienia tutaj
