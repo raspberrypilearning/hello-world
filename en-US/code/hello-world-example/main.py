65451c68081f1a3a341eeb37a5d224ae47037b16
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-10-24 13:45:08 +0100

    New translations main.py (English)

--- en-US/code/hello-world-example/main.py
+++ en-US/code/hello-world-example/main.py
@@ -16,22 +16,20 @@ fire = "🔥"
 
 # Function definitions
 def roll_dice():
-    max = input("How many sides?:")  # Wait for input from the user
-    print("That's a D", max)  # Use the number the user entered
-    roll = randint(1, int(max))  # Use max to determine the number of sides the dice has
-    print(
-        "You rolled a", roll, fire * roll
-    )  # Repeat the fire emoji to match the dice roll
+    max = input("How many sides on your dice?:")
+    print(f"That is a D {max}")
+    roll = randint(1, int(max))
+    print(f"You rolled a {roll} {fire * roll}")
 
 
 # Put code to run under here
-print("Hello", world)
-print("Welcome to", python)
-print(python, "is very good at maths!")
-print(230 * 5782**2 / 23781)  # Print the result of the sum
-print("The date and time is", datetime.now())  # Print the current date and time
+print(f"Hello {world}")
+print(f"Welcome to {python}")
+print(f"{python} is good at maths!")
+print(f"{3 * 9}")
+print(f"The date and time is {datetime.now()}")
 
 roll_dice()  # Call the roll dice function
-print("I ❤️ rainbows 🌈")
-print("Unicorns 🦄 make me 😃")
-print("I'd like to make a story 📖 with", python)
+print(f"I ❤️ rainbows 🌈")
+print(f"Unicorns 🦄 make me 😃")
+print(f"I'd like to make a story 📖 with {python}")
