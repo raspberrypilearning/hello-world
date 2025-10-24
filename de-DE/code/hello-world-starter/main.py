53a1ad6364126816e1f7c328b487d909bee52646
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-10-24 13:43:48 +0100

    New translations main.py (German)

--- de-DE/code/hello-world-starter/main.py
+++ de-DE/code/hello-world-starter/main.py
@@ -2,17 +2,19 @@ from datetime import datetime
 from random import randint
 
 # Emoji-Variablen zur Verwendung in Deinem Projekt
-welt = '🌍🌎🌏'
-python = 'Python 🐍'
-feuer = '🔥'
+world = "🌍🌎🌏"
+python = "Python 🐍"
+fire = "🔥"
 
 # Emojis zum Kopieren und Einfügen in den Code:
 # 🎊 🙌 🙌🏼 🙌🏽 🙌🏾 🙌🏿 # 😃 🕒 🎨 🎮 🔬 🎉 🕶️ 🎲 😊
-# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
+# 👩‍🦽 👩🏼‍🦽 👩🏽‍🦽 👩🏾‍🦽 👩🏿‍🦽 🧘 🧘🏼 🧘🏽 🧘🏾 🧘🏿 🙋 🙋🏼 🙋🏽 🙋🏾 🙋🏿
 # 🦄 🚀 💯 ⭐ 💛 ❤️ 📚 ⚽ 🏏 🏀 🥋 🏆 ✨ 🥺 🌈 🔥 ♻️ 🌳
 
 # Hilfreiche Zeichen:',()*_/.#
 
+
 # Funktionsdefinitionen
+
 
 # Füge hier den Code ein, der ausgeführt werden soll
