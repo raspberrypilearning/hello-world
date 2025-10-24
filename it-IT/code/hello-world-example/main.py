306586c80f1cca5d4d383fcba203ceefaca3b517
ViFo1200 <150048602+ViFo1200@users.noreply.github.com>
2025-10-24 13:44:12 +0100

    New translations main.py (Italian)

--- it-IT/code/hello-world-example/main.py
+++ it-IT/code/hello-world-example/main.py
@@ -16,22 +16,20 @@ fire = "🔥"
 
 # Definizione delle funzioni
 def tira_dado():
-    max = input("How many sides?:")  # Wait for input from the user
-    print("That's a D", max)  # Use the number the user entered
-    risultato = randint(1, int(massimo)) # Utilizza il massimo per determinare il numero di lati del dado
-    print(
-        "You rolled a", roll, fire * roll
-    )  # Repeat the fire emoji to match the dice roll
+    max = input("How many sides on your dice?:")
+    print(f"That is a D {max}")
+    roll = randint(1, int(max))
+    print(f"You rolled a {roll} {fire * roll}")
 
 
 # Metti qui sotto il codice che verrà eseguito
-print("Hello", world)
-print("Welcome to", python)
-print(python, "is very good at maths!")
-print(230 * 5782**2 / 23781)  # Print the result of the sum
-print("The date and time is", datetime.now())  # Print the current date and time
+print(f"Hello {world}")
+print(f"Welcome to {python}")
+print(f"{python} is good at maths!")
+print(f"{3 * 9}")
+print(f"The date and time is {datetime.now()}")
 
 tira_dado() # Chiama la funzione per tirare il dado
-print("I ❤️ rainbows 🌈")
-print("Unicorns 🦄 make me 😃")
-print("I'd like to make a story 📖 with", python)
+print(f"I ❤️ rainbows 🌈")
+print(f"Unicorns 🦄 make me 😃")
+print(f"I'd like to make a story 📖 with {python}")
